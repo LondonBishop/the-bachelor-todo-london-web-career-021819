fb8d78712fa9d4294b31fb39e87894eff58bfb1c
HARRY BHATT <bob@brahmingate.co.uk>
2019-02-16 10:05:40 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,4 +1,12 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
+
+  seasonArray = data[season]
+  seasonArray.each {|element| element=="winner"}
+  
+  
+  binding.pry  
   # code here
 end
 
