f9e719c62a6574faf387d921fc85d81aabf2bb11
HARRY BHATT <bob@brahmingate.co.uk>
2019-02-16 10:21:53 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -4,13 +4,7 @@ def get_first_name_of_season_winner(data, season)
 seasonArray = data[season]
 
   seasonArray.each do |element| 
-	  element.each do |key, value|
-	    binding.pry 
-		  if key == :status && value == "Winner"
-		    binding.pry
-			  winnerName = :name
-		  end
-	  end
+    
 	end
 	binding.pry
  
